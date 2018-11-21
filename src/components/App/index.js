c2e9a953566567e0a81a1dfe5ea7728c962c9250
Mikaela Gurney <354@holbertonschool.com>
2018-11-21 13:53:23 -0800

    Refactor file organization, add error catching, pagination, and fix setstate on unmounted component

---
+++ src/components/App/index.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "../../scss/main.scss";
+
+import Table from "../Table";
+import Search from "../Search";
+import Button from "../Button";
+
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from "../../constants";
+
+//// APP COMPONENT ////
+class App extends Component {
+  _isMounted = false;
+
+  state = {
+    results: null,
+    searchKey: "",
+    searchTerm: DEFAULT_QUERY,
+    error: null
+  };
+
+  componentDidMount() {
+    this._isMounted = true;
+
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+  needsToSearchTopStories = searchTerm => {
+    return !this.state.results[searchTerm];
+  };
+
+  setSearchTopStories = result => {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedHits, page } }
+    });
+  };
+
+  onSearchSubmit = e => {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    e.preventDefault();
+  };
+
+  fetchSearchTopStories = (searchTerm, page = 0) => {
+    axios(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMoutned && this.setState({ error }));
+  };
+
+  onDismiss = id => {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedHits, page } }
+    });
+  };
+
+  onSearchChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  render() {
+    const { searchTerm, results, searchKey, error } = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
+
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong</p>
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
+        )}
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
+            More
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
